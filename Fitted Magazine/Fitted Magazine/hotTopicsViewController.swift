ec0737d4913eb445a9697aff029fb1b453da07f3
King-Kyron <kyronloggins@gmail.com>
2017-08-04 14:07:45 -0700

    First Version

---
+++ Fitted Magazine/Fitted Magazine/hotTopicsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  hotTopicsViewController.swift
+//  Fitted Magazine
+//
+//  Created by Kyron Loggins on 8/4/17.
+//  Copyright © 2017 Kyron Loggins (HiddenGenius). All rights reserved.
+//
+
+import UIKit
+
+class hotTopicsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
